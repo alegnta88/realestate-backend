a9ea4ea36abc231d17f85f1a5cee45a6fd9c836e
alegnta88 <alegnta88@gmail.com>
2025-11-24 21:15:18 +0300

    add few changes

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-import express from "express";
+import express from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
