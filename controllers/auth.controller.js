f322bd2f4a35c1f13024bb4dbf7924eec8dbe935
alegnta88 <alegnta88@gmail.com>
2025-11-25 10:40:16 +0300

    rename config

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import bcrypt from 'bcrypt';
 import prisma from '../prisma.config.js';
+
 export const registerUser = async (req, res) => {
   try {
     const { username, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
-
     const newUser = await prisma.user.create({
+      
       data: {
         username, email, password: hashedPassword
       },
