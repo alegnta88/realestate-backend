58915870a18ce5789138fa83777b4f9a7932a51a
alegnta88 <alegnta88@gmail.com>
2025-11-25 08:58:40 +0300

    update prisma

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,17 @@
 import bcrypt from 'bcrypt';
-
+import prisma from '../prisma.config.js';
 export const registerUser = async (req, res) => {
   try {
     const { username, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(username, email, hashedPassword);
+
+    const newUser = await prisma.user.create({
+      data: {
+        username, email, password: hashedPassword
+      },
+    });
+    console.log('User registered:', newUser);
+    
   } catch (error) {
     res.status(500).json({ message: 'Error registering user', error });
   }
